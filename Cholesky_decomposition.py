e4dd2a6f222508e85a95fef65303045e0b0e35d0
tomchuoi <45124700+tomchuoi@users.noreply.github.com>
2023-06-19 21:46:55 +0700

    Add files via upload

---
+++ Cholesky_decomposition.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def cholesky_decomposition(matrix):
+    n = len(matrix)
+    L = np.zeros((n, n))
+
+    for i in range(n):
+        for j in range(i+1):
+            if i == j:
+                L[i][j] = np.sqrt(matrix[i][i] - np.sum(L[i][:j]**2))
+            else:
+                L[i][j] = (matrix[i][j] - np.dot(L[i][:j], L[j][:j])) / L[j][j]
+
+    return L
+
+# Read matrix from input file
+with open("matrix.txt", "r") as file:
+    lines = file.readlines()
+
+matrix = []
+for line in lines:
+    row = [float(x) for x in line.strip().split()]
+    matrix.append(row)
+
+# Convert matrix to NumPy array
+A = np.array(matrix)
+
+# Perform Cholesky decomposition
+L = cholesky_decomposition(A)
+
+
+print("Lower Triangular Matrix (L):")
+print(L)
+
+print("\nTranspose of Lower Triangular Matrix (L^T):")
+print(L.T)
